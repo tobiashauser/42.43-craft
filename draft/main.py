30a756360938f01e34db8907fa3392087bb68a0b
Tobias Hauser <apps.hausertobias@gmail.com>
2023-07-18 12:52:36 +0200

    Removed test subcommand

--- draft/main.py
+++ draft/main.py
@@ -6,12 +6,6 @@ from draft.new.main import app as new
 app = typer.Typer(no_args_is_help=True)
 
 
-@app.command("test")
-def test():
-    print("Hello, world!")
-    print("Hello, Pluto!")
-
-
 app.add_typer(new, name="new", help="Create a new document.")
 app.add_typer(
     debug,
