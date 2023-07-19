02258a9e56651f8e96aa1a43c4e5cfa9877f2f7a
Tobias Hauser <apps.hausertobias@gmail.com>
2023-07-19 22:33:02 +0200

    Initial clean up

---
+++ _craft_documents/main.py
@@ -0,0 +1,16 @@
+import typer
+
+from craft_documents.debug.main import app as debug
+from craft_documents.new.main import app as new
+from craft_documents.templates.main import app as templates
+
+app = typer.Typer(no_args_is_help=True)
+
+
+app.add_typer(new, name="new", help="Create a new document.")
+app.add_typer(
+    debug,
+    name="debug",
+    help="Output the configuration in with which the tool would run.",
+)
+app.add_typer(templates, name="templates", help="Manage the templates directory.")
--- craft_documents/main.py
+++ craft_documents/main.py
@@ -1,16 +1,8 @@
 import typer
 
-from craft_documents.debug.main import app as debug
-from craft_documents.new.main import app as new
-from craft_documents.templates.main import app as templates
-
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer()
 
 
-app.add_typer(new, name="new", help="Create a new document.")
-app.add_typer(
-    debug,
-    name="debug",
-    help="Output the configuration in with which the tool would run.",
-)
-app.add_typer(templates, name="templates", help="Manage the templates directory.")
+@app.command()
+def main():
+    print("Hello, world!")
